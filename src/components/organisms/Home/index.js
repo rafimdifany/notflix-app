5e63a1e890ba97c9a88fde866bc7f4263a09b0f7
rafimdifany <rafimdifany@gmail.com>
2022-04-28 11:35:58 +0700

    create manga list app

--- src/components/organisms/Home/index.js
+++ src/components/organisms/Home/index.js
@@ -5,9 +5,9 @@ import SpinnerLoad from '../../atoms/spinner-load';
 import CardManga from '../../molecules/card';
 
 const Home = () => {
-  const [page, setPage] = useState(1);
   const [items, setItems] = useState([]);
-  const [schedules, setSchedules] = useState([]);
+  const [mangas, setManga] = useState([]);
+  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
   const rowCard = 3;
 
@@ -16,18 +16,17 @@ const Home = () => {
     axios
       .get(`https://api.jikan.moe/v4/manga?page=${page}&limit=12`)
       .then(function (response) {
-        // handle success
         console.log(response.data)
-        setSchedules(response.data.data);
+        setManga(response.data.data);
 
         let pageArray = [];
+        
         for (let number = 1; number <= response.data.pagination.items.count; number++) {
           pageArray.push(number);
         }
         setItems(pageArray);
       })
       .catch(function (error) {
-        // handle error
         console.log(error);
       })
       .finally(() => {
@@ -37,15 +36,13 @@ const Home = () => {
 
 
   return (
-    <>
     <div className='mt-3'>
       <Container>
         <Row>
-        {loading ? (
-          <SpinnerLoad />
-          ) : (
-            schedules.length > 0 &&
-            schedules.map((data, index) => (
+        {
+          loading ? (<SpinnerLoad />) : (
+            mangas.length > 0 &&
+            mangas.map((data, index) => (
               <Col md={3} key={index}>
                 <CardManga
                 title={data.title}
@@ -58,20 +55,21 @@ const Home = () => {
                 />
               </Col>
             ))
-          )}
+          )
+        }
         </Row>
 
         <Pagination>
-                {items.length > 0 &&
-                  items.map((data) => (
-                    <Pagination.Item key={data} active={data === page} onClick={() => setPage(data)}>
-                      {data}
-                    </Pagination.Item>
-                  ))}
-              </Pagination>
+          {
+            items.length > 0 && items.map((data) => (
+              <Pagination.Item key={data} active={data === page} onClick={() => setPage(data)}>
+                {data}
+              </Pagination.Item>
+            ))
+          }
+        </Pagination>
       </Container>
     </div>
-    </>
   );
 };
 
